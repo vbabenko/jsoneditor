8a397d31a9988ef2bd67d6f801365116ee48c845
vladyslav <babenko.vladyslav@gmail.com>
2017-07-01 16:53:53 +0300

    - built version

--- dist/jsoneditor.js
+++ dist/jsoneditor.js
@@ -9566,7 +9566,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      // Activate autocomplete
 	      setTimeout(function (hnode, element) {
 	        if (element.innerText.length > 0) {
-	          var result = this.options.autocomplete.getOptions(element.innerText, editor.get(), jsonElementType, hnode.getPath());
+	          var result = this.options.autocomplete.getOptions(element.innerText, this.get(), jsonElementType, hnode.getPath());
 	          if (typeof result.then === 'function') {
 	            // probably a promise
 	            if (result.then(function (obj) {
